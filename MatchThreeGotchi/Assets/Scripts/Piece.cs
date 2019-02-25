4a452794f019821339c373a34f66b113e006671f
TimmPure <T.j.m.voskuil@gmail.com>
2019-02-25 16:53:25 +0100

    Pieces can be selected

--- MatchThreeGotchi/Assets/Scripts/Piece.cs
+++ MatchThreeGotchi/Assets/Scripts/Piece.cs
@@ -6,6 +6,11 @@ public class Piece : MonoBehaviour {
 
     private Sprite flavour;
     private SpriteRenderer sr;
+
+    private static Piece previousSelected = null;
+    private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+    private bool isSelected = false;
+
     public int row;
     public int col;
 
@@ -27,5 +32,37 @@ public class Piece : MonoBehaviour {
     {
         sr = this.gameObject.GetComponent<SpriteRenderer>();
     }
-    
+
+    private void OnMouseDown()
+    {
+        if (isSelected)
+        {
+            //This Piece is already selected and clicked again; deselect it
+            Deselect();
+        } else {
+            if (previousSelected == null)
+            {
+                //There is no Piece selected yet; select it
+                Select();
+            } else {
+                //This is the second piece selected; we deselect the first one and swap if adjacent
+                //TODO: Swap here!
+                previousSelected.Deselect();
+            }
+        }
+    }
+
+    private void Select()
+    {
+        isSelected = true;
+        sr.color = selectedColor;
+        previousSelected = this;
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        sr.color = Color.white;
+        previousSelected = null;
+    }
 }
