e4fce3675eefacd9e581c6567ec3e2cf872245e8
TimmPure <T.j.m.voskuil@gmail.com>
2019-02-25 15:14:44 +0100

    Tiles know their neighbours

--- MatchThreeGotchi/Assets/Scripts/BoardController.cs
+++ MatchThreeGotchi/Assets/Scripts/BoardController.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class BoardController : MonoBehaviour {
 
     public Sprite[] flavours;
+    public GameObject tilePrefab;
+    public GameObject[,] grid;
     public static int cols = 9;
     public static int rows = 9;
-    public GameObject[,] grid;
-    public GameObject tilePrefab;
     public static BoardController instance;
 
 	void Start () {
         instance = this;
         grid = new GameObject[cols, rows];
-
+        FillGridWithTiles();
+        SetTileNeighbours();
+	}
+	
+	void FillGridWithTiles()
+    {
         for (int i = 0; i < cols; i++)
         {
             for (int j = 0; j < rows; j++)
             {
-
-                GameObject obj = Instantiate(tilePrefab,new Vector2(i,j),Quaternion.identity,this.gameObject.transform) as GameObject;
+                GameObject obj = Instantiate(tilePrefab, new Vector2(i, j), Quaternion.identity, this.gameObject.transform) as GameObject;
                 obj.name = "( " + i + " , " + j + " )";
                 obj.GetComponent<SpriteRenderer>().sprite = flavours[Random.Range(0, flavours.Length)];
                 grid[i, j] = obj;
             }
         }
-	}
-	
-	void Update () {
-		
-	}
+    }
+
+    void SetTileNeighbours()
+    {
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                if (i > 0)
+                {
+                grid[i, j].GetComponent<GridTile>().LeftNeighbour = grid[i - 1, j].GetComponent<GridTile>();
+                }
+                if(i < cols -1)
+                {
+                grid[i, j].GetComponent<GridTile>().RightNeighbour = grid[i + 1, j].GetComponent<GridTile>();
+                }
+                if(j < rows -1)
+                {
+                grid[i, j].GetComponent<GridTile>().UpNeighbour = grid[i, j + 1].GetComponent<GridTile>();
+                }
+                if(j > 0)
+                {
+                grid[i, j].GetComponent<GridTile>().DownNeighbour = grid[i, j - 1].GetComponent<GridTile>();
+                }
+            }
+        }
+    }
 }
--- MatchThreeGotchi/Assets/Scripts/GridTile.cs
+++ MatchThreeGotchi/Assets/Scripts/GridTile.cs
@@ -4,13 +4,88 @@ using UnityEngine;
 
 public class GridTile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GridTile leftNeighbour;
+    private GridTile rightNeighbour;
+    private GridTile upNeighbour;
+    private GridTile downNeighbour;
+    private int row;
+    private int col;
+
+    public GridTile LeftNeighbour
+    {
+        get
+        {
+            return leftNeighbour;
+        }
+
+        set
+        {
+            leftNeighbour = value;
+        }
+    }
+
+    public GridTile RightNeighbour
+    {
+        get
+        {
+            return rightNeighbour;
+        }
+
+        set
+        {
+            rightNeighbour = value;
+        }
+    }
+
+    public GridTile UpNeighbour
+    {
+        get
+        {
+            return upNeighbour;
+        }
+
+        set
+        {
+            upNeighbour = value;
+        }
+    }
+
+    public GridTile DownNeighbour
+    {
+        get
+        {
+            return downNeighbour;
+        }
+
+        set
+        {
+            downNeighbour = value;
+        }
+    }
+
+    public int Row
+    {
+        get
+        {
+            return row;
+        }
+
+        set
+        {
+            row = value;
+        }
+    }
+
+    public int Col
+    {
+        get
+        {
+            return col;
+        }
+
+        set
+        {
+            col = value;
+        }
+    }
 }
