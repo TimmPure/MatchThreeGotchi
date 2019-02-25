77f23ba2db653488496cd294d4ddf78be64df952
TimmPure <T.j.m.voskuil@gmail.com>
2019-02-25 14:32:03 +0100

    Spawn grid of random Tiles that have donut sprites

---
+++ MatchThreeGotchi/Assets/Scripts/BoardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    public Sprite[] flavours;
+    public static int cols = 9;
+    public static int rows = 9;
+    public GameObject[,] grid;
+    public GameObject tilePrefab;
+    public static BoardController instance;
+
+	void Start () {
+        instance = this;
+        grid = new GameObject[cols, rows];
+
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+
+                GameObject obj = Instantiate(tilePrefab,new Vector2(i,j),Quaternion.identity,this.gameObject.transform) as GameObject;
+                obj.name = "( " + i + " , " + j + " )";
+                obj.GetComponent<SpriteRenderer>().sprite = flavours[Random.Range(0, flavours.Length)];
+                grid[i, j] = obj;
+            }
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ MatchThreeGotchi/Assets/Scripts/CameraCenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCenter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.orthographicSize = BoardController.rows / 2f;
+        this.transform.position = new Vector3(BoardController.cols / 2f - 0.5f, BoardController.rows / 2f -0.5f, -10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
